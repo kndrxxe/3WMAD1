8cf6caa562e36ffdece76a3d9153111dbf81b8a5
Kendrix Brosas <brosaskndrx05@fmail.com>
2023-07-31 16:05:41 +0800

    Commit

--- index.php
+++ index.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <style type="text/css" media="all">
-      h1 {
-        color: red;
-      }
-    </style>
 </head>
 <body>
     <h1>Kendrix Brosas</h1>
